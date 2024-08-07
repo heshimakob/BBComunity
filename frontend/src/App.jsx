07c0df0864869474e7310964180cf28c44e0b84c
heshimakob <heshimakob@gmail.com>
2024-08-07 15:49:24 +0200

    choice users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router  ,Routes , Route } from "react-router-dom"
 import Home from '../src/components/Home'
+import ChooseUser from "./components/ChooseUser"
 function App() {
 
 
@@ -7,6 +8,7 @@ function App() {
   <Router>
     <Routes>
         <Route path="/" element= {<Home/>} />
+        <Route path="/choose-user" element= {<ChooseUser/>} />
     </Routes>
   </Router>
   )
