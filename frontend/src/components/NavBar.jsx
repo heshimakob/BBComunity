22b8de6b5d08a857516ee1823928943e9a7212ee
heshimakob <heshimakob@gmail.com>
2024-08-10 09:14:11 +0200

    #

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+// import B from "../assets/B.png"
+// import h from "../assets/h.jpg"
+import avatar2 from "../assets/avatar2.png"
+import avatar from "../assets/avatar.png"
 
 function NavBar() {
   return (
     <>
     <section className="overflow-hidden">
-<div className="bg-black">
+<div className="bg-white">
 <div className="container px-4 mx-auto">
 <div className="flex items-center justify-between py-5">
 <div className="w-auto">
 <div className="flex flex-wrap items-center">
 <div className="w-auto pr-2">
   <a href="#">
-    {/* <img src="basko-assets/logos/basko-logo-light.png" alt /> */}
+    <img src="basko-assets/logos/basko-logo-light.png" alt />
   </a>
 </div>
 </div>
@@ -20,10 +24,10 @@ function NavBar() {
 <div className="flex flex-wrap items-center">
 <div className="w-auto hidden lg:block">
   <ul className="flex items-center mr-8">
-    <li className="mr-14 font-medium text-white hover:text-gray-200 tracking-tight"><a href="#">Features</a></li>
-    <li className="mr-14 font-medium text-white hover:text-gray-200 tracking-tight"><a href="#">Pricing</a></li>
-    <li className="mr-8 font-medium text-white hover:text-gray-200 tracking-tight border-r border-gray-700 pr-8"><a href="#">Automation</a></li>
-    <li className="font-medium text-white hover:text-gray-200 tracking-tight"><a href="#">Customer Login</a></li>
+    <li className="mr-14 font-medium text-black hover:text-gray-200 tracking-tight"><a href="#">Features</a></li>
+    <li className="mr-14 font-medium text-black hover:text-gray-200 tracking-tight"><a href="#">Pricing</a></li>
+    <li className="mr-8 font-medium text-black hover:text-gray-200 tracking-tight border-r border-gray-700 pr-8"><a href="#">Automation</a></li>
+    <li className="font-medium text-black hover:text-gray-200 tracking-tight"><a href="#">Customer Login</a></li>
   </ul>
 </div>
 <div className="w-auto hidden lg:block">
@@ -74,15 +78,15 @@ function NavBar() {
 </nav>
 </div>
 </div>
-<div className="pt-16 lg:pt-32 pb-24 lg:pb-52 bg-black overflow-hidden">
+<div className="pt-16 lg:pt-32 pb-24 lg:pb-52 bg-white overflow-hidden">
 <div className="container px-4 mx-auto">
 <div className="flex flex-wrap -m-8">
 <div className="w-full md:w-1/2 p-8">
 <div className="md:max-w-lg">
-<span className="inline-block mb-8 px-3 py-1 text-white font-semibold bg-gray-600 rounded-full">#1 Brand Building Tool</span>
-<h1 className="font-heading mb-12 text-6xl md:text-7xl text-white tracking-tighter">Entreprendre dans les ICC</h1>
+<h1 className="inline-block mb-8 px-3 py-1 text-black font-semibold bg-gray-600 rounded-full">#Black Born Community bootcamp</h1>
+<h1 className="font-heading mb-12 text-5xl md:text-6xl text-black tracking-tighter">Learn, create, build and connect...</h1>
 <div className="flex flex-wrap -m-2 mb-14">
-  <div className="w-auto p-2"><a className="inline-block px-5 py-4 text-white font-semibold tracking-tight bg-indigo-500 hover:bg-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-400 transition duration-200" href="#">Try 14 Days Free Trial</a></div>
+  <div className="w-auto p-2"><a className="inline-block px-5 py-4 text-black font-semibold tracking-tight bg-indigo-500 hover:bg-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-400 transition duration-200" href="#">Try 14 Days Free Trial</a></div>
   <div className="w-auto p-2">
     <a className="inline-flex items-center px-5 py-4 bg-transparent hover:bg-indigo-600 border border-gray-600 hover:border-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-300 transition duration-200" href="#">
       <div className="inline-block">
@@ -90,7 +94,7 @@ function NavBar() {
           <path d="M3 12.5L3 13.25C3 14.4926 4.00736 15.5 5.25 15.5L12.75 15.5C13.9926 15.5 15 14.4926 15 13.25L15 12.5M12 9.5L9 12.5M9 12.5L6 9.5M9 12.5L9 3.5" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </div>
-      <span className="text-white font-semibold tracking-tight">Download IOS App</span>
+      <span className="text-black font-semibold tracking-tight">Download IOS App</span>
     </a>
   </div>
 </div>
@@ -99,18 +103,18 @@ function NavBar() {
     <div className="w-auto p-1.5">
       <div className="flex flex-wrap">
         <div className="w-auto">
-          {/* <img src="basko-assets/images/headers/avatar.png" alt /> */}
+          <img src={avatar} alt />
         </div>
         <div className="w-auto -ml-5">
-          <img src="basko-assets/images/headers/avatar2.png" alt />
+          <img src={avatar2} alt />
         </div>
         <div className="w-auto -ml-5">
-          <img src="basko-assets/images/headers/avatar3.png" alt />
+          <img src={avatar} alt />
         </div>
       </div>
     </div>
     <div className="flex-1 p-1.5">
-      <p className="text-white tracking-tight">Join other 12k+ Marketers and start building brands reputation now.</p>
+      <p className="text-black tracking-tight">Join our community  12k+ Remote is today.</p>
     </div>
   </div>
 </div>
