1715c3be7d864cb0e80bd10dc6c4a5a489e4b0de
heshimakob <heshimakob@gmail.com>
2024-08-14 21:16:59 +0200

    add

--- frontend/src/redux/actions/courseActions.js
+++ frontend/src/redux/actions/courseActions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const addCours =(user)=>async dispatch=>{
-dispatch({type:'A_REGISTER_REQUEST'})
+dispatch({type:'ADD_COURS_REQUEST'})
 try {
     
     const res= await axios.post('/api/cours/addcours',user)
     console.log(res)
-    dispatch ({type:'USER_REGISTER_SUCCESS'})
+    dispatch ({type:'ADD_COURS_SUCCESS'})
 } catch (error) {
-    dispatch ({type:'USER_REGISTER_FAIL', payload:error})
+    dispatch ({type:'ADD_COURS_FAIL', payload:error})
     
 }
 
