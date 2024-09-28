8da6408d92f54a74de0ad3b06d71e370afd56ed0
heshimakob <heshimakob@gmail.com>
2024-09-28 14:38:29 +0200

    link company

--- frontend/src/components/Why.jsx
+++ frontend/src/components/Why.jsx
@@ -6,6 +6,7 @@ import equipe from "../assets/equipe.webp"
 import ai from "../assets/ai.jpg"
 import Informations from './Informations';
 import {motion} from "framer-motion"
+import { Link } from 'react-router-dom';
 
 const  Why =({setProgress})=>{
   useEffect(()=>{
@@ -34,7 +35,7 @@ const  Why =({setProgress})=>{
 
     <h1 className="mb-4 text-3xl  text-center font-extrabold text-black dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Nous sommes une communaute</span></h1>
     <div className=" bbc-container  flex justify-center items-center">
-  <div className=" w-auto p-2"><a className="inline-block px-5 py-4 text-white font-semibold tracking-tight bg-indigo-800 hover:bg-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-400 transition duration-200" href="/company">Engager un talent</a></div>
+  <div className=" w-auto p-2"><Link className="inline-block px-5 py-4 text-white font-semibold tracking-tight bg-indigo-800 hover:bg-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-400 transition duration-200" to="/company" > Engager un talent</Link></div>
   <div className="w-auto p-2">
     {/* <a className="inline-flex items-center px-5 py-4 bg-gradient-to-r to-emerald-600 from-sky-400 hover:bg-indigo-800 border border-gray-800 hover:border-indigo-600 rounded-lg focus:ring-4 focus:ring-indigo-300 transition duration-200" href="#">
       <div className="inline-block">
