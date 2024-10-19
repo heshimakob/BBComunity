ea9fcbd0fcb2a856a823bfb642efac73584e20ff
heshimakob <heshimakob@gmail.com>
2024-10-19 11:56:39 +0200

    call for action section update

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-
+import three from "../assets/background/three-iphones.png"
+import { Link } from 'react-router-dom'
 const Actions = () => {
   return (
       <>
-                <section className="py-20">
+                         <section className="py-20">
   <div className="container px-4 mx-auto">
     <div className="pt-20 pb-24 px-8 md:px-16 bg-blue-600 rounded">
       <div className="flex flex-wrap -mx-8">
         <div className="w-full lg:w-1/2 px-8 flex">
           <div className="my-auto">
-            <h2 className="text-3xl lg:text-4xl font-semibold text-white">Lorem ipsum dolor sit amet consectetur domor</h2>
-            <p className="mt-2 mb-6 text-blueGray-50 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus eget justo et iaculis.</p>
-            <a className="inline-block py-4 px-8 text-xs border bg-white text-blue-600 font-semibold leading-none hover:bg-blue-600 hover:border-white hover:text-white rounded transition duration-300" href="#">Get Started</a>
+            <h2 className="text-3xl lg:text-4xl font-semibold text-white">Rejoignez-nous dès aujourd'hui !</h2>
+            <p className="mt-2 mb-6 text-blueGray-50 leading-loose">Inscrivez-vous pour découvrir nos formations en ligne et sur mesure qui vous aideront à atteindre vos objectifs. Ne manquez pas cette opportunité de faire avancer votre carrière !</p>
+            <Link className="inline-block py-4 px-8 text-xs border bg-white text-blue-600 font-semibold leading-none hover:bg-blue-600 hover:border-white hover:text-white rounded transition duration-300" to="/signin">Se Connecter</Link>
           </div>
         </div>
         <div className="w-full lg:w-1/2 px-8 mt-12 lg:mt-0">
@@ -25,7 +26,7 @@ const Actions = () => {
             <div className="absolute bg-blue-600" style={{top: '12.8%', left: '70%', width: '25.2%', height: '72%'}}>
               <img className="object-cover w-full h-full" src="https://images.unsplash.com/photo-1535223289827-42f1e9919769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" alt />
             </div>
-            <img className="relative mx-auto" src="metis-assets/elements/three-iphones.png" alt />
+            <img className="relative mx-auto" src={three} alt />
           </div>
         </div>
       </div>
--- frontend/src/components/Formules.jsx
+++ frontend/src/components/Formules.jsx
@@ -3,6 +3,10 @@ import Footer from './Footer'
 import NavBar from './NavBar'
 import intersect  from "../assets/background/intersect.svg"
 import { Link } from 'react-router-dom'
+import people from "../assets/illustration/people.png"
+import job from "../assets/illustration/job-interview.png"
+import team from "../assets/illustration/team-management.png"
+import Actions from './Actions'
 
 const Formules = () => {
   return (
@@ -10,21 +14,28 @@ const Formules = () => {
     <NavBar/>
     <section className="py-20 xl:bg-contain bg-top bg-no-repeat" style={{backgroundImage: `url(${intersect})`}}>
 <div className="container px-4 mx-auto">
+
+
+
 <div className="text-center mb-16 mt-20">
 <h2 className="max-w-lg mx-auto mb-4 text-4xl font-bold font-heading">
-<span>Start saving time today and</span>
-<span className="text-blue-600">choose</span>
-<span>your best plan</span>
+<span>Commencez à optimiser votre temps dès aujourd'hui  </span>
+<span className="text-blue-600">et
+découvrez nos  </span>
+<span>formations adaptées à vos besoins !</span>
 </h2>
-<p className="max-w-sm mx-auto text-lg text-blueGray-400">Best for freelance developers who need to save their time</p>
+<p className="max-w-sm mx-auto text-lg text-blueGray-400">Conçu pour les apprenants de tous niveaux, offrant des programmes en ligne et des options personnalisées.</p>
 </div>
+
+
+
 <div className="flex flex-wrap -mx-3">
 <div className="w-full md:w-1/2 lg:w-1/3 px-3 mb-6">
 <div className="pt-16 pb-8 px-4 text-center bg-white rounded shadow">
-<img className="h-20 mb-6 mx-auto" src="metis-assets/illustrations/people.png" alt />
-<h3 className="mb-2 text-4xl font-bold font-heading">BootCamp</h3>
+<img className="h-20 mb-6 mx-auto" src={people} alt />
+<h3 className="mb-2 text-4xl font-bold font-heading">BootCamp en ligne</h3>
 <span className="text-4xl text-blue-600 font-bold font-heading">$50</span>
-<p className="mt-2 mb-8 text-blueGray-400">user per month</p>
+<p className="mt-2 mb-8 text-blueGray-400">Sur 4 mois de bootcamp</p>
 <div className="flex flex-col items-center mb-8">
 <ul className="text-blueGray-400">
   <li className="flex mb-3">
@@ -32,6 +43,18 @@ const Formules = () => {
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
     <span>4 mois</span>
+  </li>
+  <li className="flex mb-3">
+    <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    <span>15 projets </span>
+  </li>
+  <li className="flex mb-3">
+    <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    <span>4h A 7heures de travail par jours </span>
   </li>
   <li className="flex mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -53,86 +76,89 @@ const Formules = () => {
   </li> */}
 </ul>
 </div>
-<div><Link className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-white text-center font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded" to="/register">S'inscrire</Link><a className="block sm:inline-block py-4 px-6 text-xs text-blueGray-500 hover:text-blueGray-600 text-center font-semibold leading-none bg-white border border-blueGray-200 hover:border-blueGray-300 rounded" href="#">Purchase</a></div>
+<div><Link className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-white text-center font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded" to="/register">S'inscrire</Link></div>
 </div>
 </div>
 <div className="w-full md:w-1/2 lg:w-1/3 px-3 mb-6">
 <div className="pt-16 pb-8 px-4 text-center text-white bg-blueGray-800 rounded shadow">
-<img className="h-20 mb-6 mx-auto" src="metis-assets/illustrations/job-interview.png" alt />
-<h3 className="mb-2 text-4xl font-bold font-heading">Entreprises</h3>
-<span className="text-4xl font-bold font-heading">$500</span>
-<p className="mt-2 mb-8">Pour un mois de formation</p>
+<img className="h-20 mb-6 mx-auto" src={job} alt />
+<h3 className="mb-2 text-4xl font-bold font-heading">Bootcamp sur site</h3>
+<span className="text-4xl font-bold font-heading">$312.5</span>
+<p className="mt-2 mb-8">Sur 3 mois de bootcamp</p>
 <div className="flex flex-col items-center mb-8">
 <ul>
   <li className="flex items-center mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>6 Emails</span>
+    <span>Une connexion wifi</span>
   </li>
   <li className="flex items-center mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>30 modules</span>
+    <span>2 heure de formation(8h-10h, 16-18h)</span>
   </li>
   <li className="flex items-center mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>Compte illimites</span>
+    <span>Un espace de travail de haute qualite</span>
   </li>
   <li className="flex items-center">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>Avec assitance technique par proposition</span>
+    <span>Avec assitance technique</span>
   </li>
+  
+
 </ul>
 </div>
-<div><a className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-blue-600 text-center font-semibold leading-none bg-white hover:bg-blueGray-50 rounded" href="#">Start Free Trial</a><a className="block sm:inline-block py-4 px-6 text-xs text-white text-center font-semibold leading-none border border border-blueGray-500 hover:border-blueGray-400 rounded" href="#">Purchase</a></div>
+<div><a className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-blue-600 text-center font-semibold leading-none bg-white hover:bg-blueGray-50 rounded" href="#">inscription@bbcommunity.academy</a></div>
 </div>
 </div>
 <div className="w-full lg:w-1/3 px-3 mb-6">
 <div className="pt-16 pb-8 px-4 text-center bg-white rounded shadow">
-<img className="h-20 mb-6 mx-auto" src="metis-assets/illustrations/team-management.png" alt />
-<h3 className="mb-2 text-4xl font-bold font-heading">Professional</h3>
-<span className="text-4xl text-blue-600 font-bold font-heading">$62.99</span>
-<p className="mt-2 mb-8 text-blueGray-400">user per month</p>
+<img className="h-20 mb-6 mx-auto" src={team} alt />
+<h3 className="mb-2 text-4xl font-bold font-heading">Programme de bootCamp sur mesure</h3>
+<span className="text-4xl text-blue-600 font-bold font-heading">$437.5</span>
+<p className="mt-2 mb-8 text-blueGray-400">Sur 2 mois de bootcamp</p>
 <div className="flex flex-col items-center mb-8">
 <ul className="text-blueGray-400">
   <li className="flex mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>12 Emails</span>
+    <span>Choix de l'heure de la formation</span>
   </li>
   <li className="flex mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>8 Datebases</span>
+    <span>2 heures de formation par jours</span>
   </li>
   <li className="flex mb-3">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>Unlimited Domains</span>
+    <span>Espace de travail et cafe offert</span>
   </li>
   <li className="flex">
     <svg className="w-6 h-6 mr-2 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span>50 GB Storage</span>
+    <span>Assistance technique </span>
   </li>
 </ul>
 </div>
-<div><a className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-white text-center font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded" href="#">Start Free Trial</a><a className="block sm:inline-block py-4 px-6 text-xs text-blueGray-500 hover:text-blueGray-600 text-center font-semibold leading-none bg-white border border-blueGray-200 hover:border-blueGray-300 rounded" href="#">Purchase</a></div>
+<div><a className="block sm:inline-block py-4 px-6 mb-4 sm:mb-0 sm:mr-3 text-xs text-white text-center font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded" href="#">inscription@bbcommunity.academy</a></div>
 </div>
 </div>
 </div>
 </div>
 </section>
+<Actions/>
 
 <Footer/>
 
